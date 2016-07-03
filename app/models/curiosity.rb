72bcf07ff06880c1c8490ebd84e244f9f7cc50c6
soniaprevost <so.prevost@gmail.com>
2016-07-03 14:41:46 +0200

    Add week 5 change

--- app/models/curiosity.rb
+++ app/models/curiosity.rb
@@ -1,2 +1,10 @@
 class Curiosity < ActiveRecord::Base
+  validates :category, inclusion: { in: [
+                                          'Penderie',
+                                          'Coffre à jouets',
+                                          'Vaisselle',
+                                          'Décorations',
+                                          'Amis',
+                                          'Bibliothèque',
+                                        ]}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618174815) do
+ActiveRecord::Schema.define(version: 20160703123613) do
 
   create_table "curiosities", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160618174815) do
     t.string   "image_text"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "category"
   end
 
 end
