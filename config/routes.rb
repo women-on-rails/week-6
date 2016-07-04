2ce9f3a57040c106184da9ea98b5945a32b5169e
Sonia <so.prevost@gmail.com>
2016-07-04 13:02:27 +0200

    Add destroy exercice via view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'curiosities#index'
+
+  get 'curiosities', to: 'curiosities#index', as: :curiosities
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
